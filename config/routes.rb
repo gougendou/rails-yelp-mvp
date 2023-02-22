845aa488ce1757ca988a41f143b9280e8ff85cae
SamTreon <stat123@hotmail.co.uk>
2023-02-22 12:30:28 +0000

    added all the backend logic for adding restaurants and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # resources :restaurants, only: [:index, :show, :new, :create] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+
 end
+
+# So let’s define our minimal product here:
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+# A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+# A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
+# And that’s it!
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Faker::Config.locale = 'en-US'
+puts "creating..."
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: ["chinese", "french", "italian", "belgian", "japanses"].sample
+  )
+
+  puts "finished..."
+end
